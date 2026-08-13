8849ede2704c23fdb6cedb822541783f8ecbb303
cjxpj <2960965389@qq.com>
2026-08-14 04:50:08 +0800

    release: v16.19.1

--- nebula/appfiles/embed.go
+++ nebula/appfiles/embed.go
@@ -28,4 +28,4 @@ func GetFile(filename string) ([]byte, error) {
 var Key []byte = []byte("cjxpj2960965389 nebula0052 juice")
 
 // 版本号
-var Version string = "16.19.0"
+var Version string = "16.19.1"
